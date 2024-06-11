de4d6c5fda2e974a82a538177da690bbdc32af07
Zoya Hammad <zoyahammadk@gmail.com>
2024-06-11 08:27:11 +0500

    Added status codes

--- httpcats/middleware.py
+++ httpcats/middleware.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 
-level200 = [i for i in range(200,205)]
-level400 = [i for i in range(400,405)] + [409]
-level500 = [i for i in range(500,205)] + [599]
+level100 = [i for i in range(100,104)]
+level200 = [i for i in range(200,209)] + [214,226]
+level300 = [i for i in range(300,306)] + [307,308]
+level400 = [i for i in range(400,419)] + [i for i in range(420,427)] + [428,429,431,444,450,451,497,498,499,]
+level500 = [i for i in range(500,512)] + [522,522,523,525,530,599]
 
 class CustomHttpCatsMiddleware:
     def __init__(self, get_response):
@@ -11,7 +13,7 @@ class CustomHttpCatsMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        if response.status_code in [level200,level400,level500]:
+        if response.status_code in [level100,level200,level300,level400,level500]:
             # Redirect to the HTTP Cats page for errors
             return redirect('httpcats:http_cat',response.status_code)
         return response

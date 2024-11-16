32fe2a0f1b0987c3cc8481387ee7d36fb5221c60
Zoya Hammad <zoyahammadk@gmail.com>
2024-11-16 08:33:42 +0500

    Committing local changes

--- books/views.py
+++ books/views.py
@@ -22,7 +22,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request,user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("books:index"))
         else:
             return render(request, "login.html", {
                 "error_message": "Invalid username and/or password."
@@ -33,7 +33,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("books:index"))
 
 
 def register(request):
